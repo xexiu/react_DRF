5399f498260cd4205ca3c834796de6d4bea6e2cc
Mironescu Iancu <mironess@emea.roche.com>
2021-12-14 10:29:25 +0100

    initial blog project frontend react / api on python backend (check django_DRF repository)

--- src/index.js
+++ src/index.js
@@ -3,10 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const Routing = () => {
+  return (
+    <Router>
+      <Header />
+        <Routes>
+            <Route path='/' element={<App />} />
+        </Routes>
+        <Footer />
+    </Router>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routing />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +30,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+ 
