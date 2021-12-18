350d69ea9fc6bcd67500aaefb8dadd945b37fc89
xexiu <sergiu.mironescu@gmail.com>
2021-12-18 20:14:10 +0100

    added deleted wrong files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
